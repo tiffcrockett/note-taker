0a985b69b048ab96ca5a3f1201549a20d80dd33b
Tiffany Crockett <crockett.tiff@gmail.com>
2021-01-31 22:40:21 -0700

    update PORT

--- Develop/server.js
+++ Develop/server.js
@@ -1,9 +1,10 @@
 
 var express = require("express");
 var path = require("path");
+var fs = require('fs');
 
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json()); 
@@ -36,4 +37,5 @@ app.post("/api/notes", function(req, res) { });
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
+
 
