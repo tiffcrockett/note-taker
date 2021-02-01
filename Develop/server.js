972f7190a98c783fee7c7fac62e6fed8011daa2c
Tiffany Crockett <crockett.tiff@gmail.com>
2021-01-31 22:09:47 -0700

    require express, set routes and port

---
+++ Develop/server.js
@@ -0,0 +1,34 @@
+
+var express = require("express");
+var path = require("path");
+
+var app = express();
+var PORT = 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json()); 
+
+
+var notes = []
+
+   
+app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "index.html"));
+}); 
+  
+app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "notes.html"));
+});
+ 
+  
+app.get("/api/notes", function(req, res) { });
+    
+
+app.post("/api/notes", function(req, res) { });
+
+
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
+
