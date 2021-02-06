c2e46a76f3fae64fd60db6f05e7b69a4f33fa52b
Tiffany Crockett <crockett.tiff@gmail.com>
2021-02-06 09:22:06 -0700

    prepend public to routes

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-var express = require("express");
-var app = express();
-
+var express = require("express"); 
+var app = express(); 
 
 var PORT = process.env.PORT || 8080;
 
 
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(express.json()); 
+app.use(express.static('public'));
 
 
 require("./routes/apiRoutes")(app);
