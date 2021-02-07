245a01eb4688cc312be804a2b5304770f4718d88
Tiffany Crockett <crockett.tiff@gmail.com>
2021-02-07 08:31:25 -0700

    correct starter code file, update uuid, update post route

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,45 +1,45 @@
-var noteData = require("../data/db.json");
-const { v4: uuidv4 } = require('uuid');
+var noteData = require("../db/db.json");
+var uuid = require('uuid-random');
 var path = require("path");
 const fs = require("fs");
+// const { rawListeners } = require("process");
 
 module.exports = function(app) { 
 
   //returns contents of noteData back to the client
   app.get("/api/notes", function(req, res) {
     res.json(noteData); 
-
+  });
   
   app.post("/api/notes", function(req, res) { 
 
-    noteData.id = uuidv4;
-    // instantiate noteData
-    const newNoteData = join(noteData, noteData.id);
-    // id
-
-    // push note and id, only write newNote
-    if(newNoteData) {
-      newNoteData.push(req.body)
-      res.json(true);
-    } 
-
-    fs.writeFileSync('../data/db.json', function (err, data) {
-      if (err) {
+    const newNoteData = req.body;
+    const noteFile = path.join(__dirname, "db/db.json");
+    noteData.id = uuid();
+    noteData.push(newNoteData);
+    
+    fs.writeFile(noteFile, JSON.stringify(noteData), (err) => {
+      if (err) 
           throw err;
-      }
-      newNotesData = data;
-      });
+            console.log('Error')
+      }); 
+
+      res.send(newNotesData);
    });
     
-  app.delete('/api/notes/:id', checkNoteExists, (req, res) => {
-    const { id } = req.params.id; 
+  app.delete('/api/notes/:id', (req, res) => {
+    const id = req.params.id; 
+    const noteFile = path.join(__dirname, "db/db.json");
 
-    const noteDataIndex = note.findIndex(n => n.id == id); 
+    const noteDataIndex = noteData.findIndex(noteData => noteData.id == id); 
+    noteData.splice(noteDataIndex, 1); 
 
-    noteData.splice(noteDataIndex, 1);
+    fs.writeFile(noteFile, JSON.stringify(noteData), (err) => {
+      if (err) 
+          throw err; 
+            console.log('Error')
+      }); 
    
     return res.send();
-    });
-  }
-)};
-
+  });
+}
--- server.js
+++ server.js
@@ -3,11 +3,9 @@ var app = express();
 
 var PORT = process.env.PORT || 3000;
 
-
+app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json()); 
-app.use(express.static('public'));
-
 
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
